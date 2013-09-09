455a2360efa688a6a567a7227a6b856bd31855f2
Jo Greybill <jogreybill@gmail.com>
2013-09-09 11:08:52 -0400

    Replaced window.load with contentLoaded

--- bar/js/university-header-full.js
+++ bar/js/university-header-full.js
@@ -81,7 +81,7 @@ var ucfhbAssignTrackingListener = function(elem, eventType, link, action, label)
 
 
 	/**
-	 * Check if data-bootstra-override has been passed to the
+	 * Check if data-bootstrap-override has been passed to the
 	 * university header script. Requires that the script tag
 	 * has an ID of 'ucfhb-script'
 	 **/
@@ -93,6 +93,58 @@ var ucfhbAssignTrackingListener = function(elem, eventType, link, action, label)
 			useBsOverride = true;
 		}
 	}
+
+	/**
+	 * contentloaded.js
+	 *
+	 * Author: Diego Perini (diego.perini at gmail.com)
+	 * Summary: cross-browser wrapper for DOMContentLoaded
+	 * Updated: 20101020
+	 * License: MIT
+	 * Version: 1.2
+	 *
+	 * URL:
+	 * http://javascript.nwbox.com/ContentLoaded/
+	 * http://javascript.nwbox.com/ContentLoaded/MIT-LICENSE
+	 *
+	 **/
+
+	// @win window reference
+	// @fn function reference
+	function contentLoaded(win, fn) {
+
+		var done = false, top = true,
+
+		doc = win.document, root = doc.documentElement,
+
+		add = doc.addEventListener ? 'addEventListener' : 'attachEvent',
+		rem = doc.addEventListener ? 'removeEventListener' : 'detachEvent',
+		pre = doc.addEventListener ? '' : 'on',
+
+		init = function(e) {
+			if (e.type == 'readystatechange' && doc.readyState != 'complete') return;
+			(e.type == 'load' ? win : doc)[rem](pre + e.type, init, false);
+			if (!done && (done = true)) fn.call(win, e.type || e);
+		},
+
+		poll = function() {
+			try { root.doScroll('left'); } catch(e) { setTimeout(poll, 50); return; }
+			init('poll');
+		};
+
+		if (doc.readyState == 'complete') fn.call(win, 'lazy');
+		else {
+			if (doc.createEventObject && root.doScroll) {
+				try { top = !win.frameElement; } catch(e) { }
+				if (top) poll();
+			}
+			doc[add](pre + 'DOMContentLoaded', init, false);
+			doc[add](pre + 'readystatechange', init, false);
+			win[add](pre + 'load', init, false);
+		}
+
+	}
+
 
 	/**
 	 * Make <IE9 behave (indexOf array searching, match, trim, JSON parsing functionality).
@@ -131,10 +183,7 @@ var ucfhbAssignTrackingListener = function(elem, eventType, link, action, label)
 
 	/** 
 	 * Insert the bar into the DOM; start listening for events.
-	 *
-	 * window.onload has to wait for the DOM and all resources to
-	 * finish loading, but is the most reliable DOM readiness check
-	 * available, and doesn't rely on dumb IE hacks.
+	 * Uses contentLoaded.js to determine when the DOM is ready.
 	 **/
 	var ucfhbCreateBar = function(callback) {
 		/* Append stylesheet to head */
@@ -292,9 +341,9 @@ var ucfhbAssignTrackingListener = function(elem, eventType, link, action, label)
 		ucfhbEventListener();
 	}
 
-	window.onload = function() {
+	contentLoaded(window, function() {
 		ucfhbCreateBar(ucfhbInitialize);
-	};
+	});
 
 
 	/**

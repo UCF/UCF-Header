89d4942f30b0ad4f08d77a1532581bc50beabb3b
Jo Greybill <jogreybill@gmail.com>
2013-09-19 14:43:17 -0400

    Use colons instead of equals

--- compiler/src/config.templ.py
+++ compiler/src/config.templ.py
@@ -1,4 +1,4 @@
 config = {
-	'root_url' = 'universityheader.ucf.edu', # No protocol, no end slash
-	'ga' = '' # Google Analytics tracking number
+	'root_url': 'universityheader.ucf.edu', # No protocol, no end slash
+	'ga': '' # Google Analytics tracking number
 }

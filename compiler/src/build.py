2e1164241e1ebecdda9f343d472931d24f6e7d14
Jo Greybill <jogreybill@gmail.com>
2013-09-19 14:23:48 -0400

    Use uglipyjs instead of slimit for smaller minified js; added if __name__ == __main__ check

--- compiler/src/build.py
+++ compiler/src/build.py
@@ -5,32 +5,35 @@
 
 import os
 import json
-from slimit import minify
+import uglipyjs
 from config import config
 
 
-script_dir = os.path.dirname(__file__)
+if __name__ == '__main__':
+	script_dir = os.path.dirname(__file__)
 
-keyterms = open(os.path.join(script_dir, 'assets/keyterms.json'), 'r').read()
-markup = open(os.path.join(script_dir, 'assets/university-header-markup.js'), 'r').read()
+	keyterms = open(os.path.join(script_dir, 'assets/keyterms.json'), 'r').read()
+	markup = open(os.path.join(script_dir, 'assets/university-header-markup.js'), 'r').read()
 
-template = open(os.path.join(script_dir, 'assets/university-header-templ.js'), 'r')
-jsfull = open(os.path.join(script_dir, '../../bar/js/university-header-full.js'), 'w+')
-jsmin = open(os.path.join(script_dir, '../../bar/js/university-header.js'), 'w+')
+	template = open(os.path.join(script_dir, 'assets/university-header-templ.js'), 'r')
+	jsfull = open(os.path.join(script_dir, '../../bar/js/university-header-full.js'), 'w+')
+	jsmin = open(os.path.join(script_dir, '../../bar/js/university-header.js'), 'w+')
 
-print "Writing university-header-full.js..."
-for line in template:
-	jsfull.write(
-		line.replace('@!@GA@!@', config['ga']).replace('@!@ROOT_URL@!@', config['root_url']).replace('@!@KEYTERMS@!@', keyterms).replace('@!@MARKUP@!@', markup)
-	)
+	print "Writing university-header-full.js..."
+	for line in template:
+		jsfull.write(
+			line.replace('@!@GA@!@', config['ga']).replace('@!@ROOT_URL@!@', config['root_url']).replace('@!@KEYTERMS@!@', keyterms).replace('@!@MARKUP@!@', markup)
+		)
 
-template.close()
-jsfull.close()
+	template.close()
+	jsfull.close()
+	print "university-header-full.js saved."
 
-jsfulltext = open(os.path.join(script_dir, '../../bar/js/university-header-full.js'), 'r').read()
+	jsfulltext = open(os.path.join(script_dir, '../../bar/js/university-header-full.js'), 'r').read()
 
-print "Writing university-header.js..."
-jsmin.write(minify(jsfulltext))
+	print "Writing university-header.js..."
+	jsmin.write(uglipyjs.compile(jsfulltext))
 
-jsfull.close()
-jsmin.close()
+	jsfull.close()
+	jsmin.close()
+	print "university-header.js saved."

fa2d5afa2cf53c11b261fd3ceac8fcf70c67c0ee
Roger Wolf <roger.wolf@ucf.edu>
2013-09-19 18:23:46 -0400

    added more quickmatches to search

--- bar/data/index.php
+++ bar/data/index.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/javascript');
 $query = urlencode(strip_tags($_GET['search']));
-echo 'ucfhbSetJsonp(\''.file_get_contents('http://search.smca.ucf.edu/service.php?limit=3&search='.$query).'\');';
+echo 'ucfhbSetJsonp(\''.file_get_contents('http://webcom.dev.smca.ucf.edu/search/webroot/service.php?limit=3&search='.$query).'\');';
 ?>

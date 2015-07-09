561889c1a3a5fe5c69880a8e391bca3d8a4f886f
RJ Bruneel <rj.bruneel@ucf.edu>
2015-07-09 10:53:01 -0400

    FIxed issue where single quote is causing a JS error

--- compiler/assets/search-proxy-templ.php
+++ compiler/assets/search-proxy-templ.php
@@ -1,5 +1,5 @@
 <?php
 header('Content-Type: application/javascript');
 $query = urlencode(strip_tags($_GET['search']));
-echo 'ucfhbSetJsonp(\''.file_get_contents('@!@SEARCH_SERVICE@!@'.$query).'\');';
+echo 'ucfhbSetJsonp(\''.str_replace("'", "&#39;", file_get_contents('@!@SEARCH_SERVICE@!@'.$query)).'\');';
 ?>

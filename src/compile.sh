842687378283879b7977eb671c4d7c57f11f3541
Jo Dickson <jogreybill@gmail.com>
2022-02-22 12:00:30 -0500

    Basic setup for js linting and processing with minimal changes to existing bar js

--- src/compile.sh
+++ src/compile.sh
@@ -3,18 +3,24 @@
 set -o nounset
 set -x
 cd "$(dirname "$0")"
-source config.conf
+source ../config.conf
 NOW=$(date +"%s")
 
-# Find/replace content from markup template file into header template
-cp assets/university-header-templ.js assets/university-header-templ.js.tmp
-perl -i -ne 's/\@\!\@MARKUP\@\!\@/`cat assets\/university-header-markup.js`/e;print' assets/university-header-templ.js.tmp
-# Find/replace config variables in header template
-sed -i -e "s|@!@GA@!@|$GA|g" assets/university-header-templ.js.tmp
-sed -i -e "s|@!@ROOT_URL@!@|$ROOT_URL|g" assets/university-header-templ.js.tmp
-sed -i -e "s|@!@VERSION@!@|$NOW|g" assets/university-header-templ.js.tmp
-# Save bar/js/university-header-full.js
-mv assets/university-header-templ.js.tmp ../bar/js/university-header-full.js
+# Create temp files:
+cp js/university-header.js js/university-header.js.tmp
+cp js/university-header-full.js js/university-header-full.js.tmp
 
-# Minify university-header-full.js
-java -jar compiler.jar --js ../bar/js/university-header-full.js --js_output_file ../bar/js/university-header.js
+# Find/replace config variables in header templates
+sed -i -e "s|@!@GA@!@|$GA|g" js/university-header.js.tmp
+sed -i -e "s|@!@ROOT_URL@!@|$ROOT_URL|g" js/university-header.js.tmp
+sed -i -e "s|@!@VERSION@!@|$NOW|g" js/university-header.js.tmp
+
+sed -i -e "s|@!@GA@!@|$GA|g" js/university-header-full.js.tmp
+sed -i -e "s|@!@ROOT_URL@!@|$ROOT_URL|g" js/university-header-full.js.tmp
+sed -i -e "s|@!@VERSION@!@|$NOW|g" js/university-header-full.js.tmp
+
+# Save bar/js/university-header.js
+mv js/university-header.js.tmp ../bar/js/university-header.js
+
+# Save bar/js/university-header-full.js
+mv js/university-header-full.js.tmp ../bar/js/university-header-full.js
